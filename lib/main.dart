7998d611302583c13711e871a17033579e7276d6
yogapawitra <yogapawitra96@gmail.com>
2024-08-03 01:52:36 +0800

    up

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('MQTT Data Display'),
